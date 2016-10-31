38e847f931be3c317ec6e5acc1805bb3ea61717b
TinWu <yyuuix@gmail.com>
2016-10-31 18:18:56 +0800

    檔案建置

--- ChaominTrading/App_Start/BundleConfig.cs
+++ ChaominTrading/App_Start/BundleConfig.cs
@@ -8,24 +8,23 @@ namespace ChaominTrading
         // 如需「搭配」的詳細資訊，請瀏覽 http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+            //開啟 CDN
+            bundles.UseCdn = true;
+            //設定 CDN 目錄
+            var jqueryCdnPath = "http://code.jquery.com/jquery-latest.min.js";
+            //優先載入 CDN 位置，如 CDN 失效將使用本機位置提供資源
+            bundles.Add(new ScriptBundle("~/bundles/jquery",
+                        jqueryCdnPath).Include(
                         "~/Scripts/jquery-{version}.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
-
-            // 使用開發版本的 Modernizr 進行開發並學習。然後，當您
-            // 準備好實際執行時，請使用 http://modernizr.com 上的建置工具，只選擇您需要的測試。
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
-
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+            bundles.Add(new ScriptBundle("~/bundles/js").Include(
+                      "~/Content/js/common.js",
+                      "~/Content/js/tab.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/css/vendor/icomoon.css",
+                      "~/Content/css/grid.css",
+                      "~/Content/css/page.css"));
         }
     }
 }
---
+++ ChaominTrading/Controllers/ProductsController.cs
@@ -0,0 +1,22 @@
+﻿using System.Web.Mvc;
+using DotWeb.Controller;
+
+namespace DotWeb.WebApp.Controllers
+{
+    public class ProductsController : WebUserController
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View("list");
+        }
+        public ActionResult list()
+        {
+            return View();
+        }
+        public ActionResult content()
+        {
+            return View();
+        }
+    }
+}
